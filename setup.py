2f1ea5a65dd7774279d0bda9be52ffaa66acf491
kiorky <kiorky@cryptelium.net>
2012-11-01 13:48:37 +0100

    reinforce build script for freebsd

--- setup.py
+++ setup.py
@@ -22,46 +22,55 @@ agg_sources_dir = os.path.join(
 files = [os.path.join('cpp', f)
          for f in os.listdir(sources_dir)
          if f.endswith('.cpp')]
+
 compilation_flags = get_compilation_flags()
-# BBB: needed for non standard pyairo.h to be found
 
-cf = ' '.join(compilation_flags['includes'])
-cf += ' %s' % os.environ.get('CFLAGS', '')
-cf = cf.replace('  ', '').strip()
-
+cf = ('%s %s' % (
+    ' '.join(compilation_flags['includes']),
+    os.environ.get('CFLAGS', ''),
+)).replace('  ', '').strip()
 cfp = cf.split()
 cfpp = []
 for idx, i in enumerate(cfp[:]):
     add = True
-    if i.startswith('-I/') and (i in cfp[idx+1:]):
-        add = False
+    if i.startswith('-I/'):
+        if i.endswith('/bin'):
+            add = False
+        if i in cfpp:
+            add = False
     if add:
         cfpp.append(i)
 cf = ' '.join(cfpp)
 
-ldf = ' '.join(compilation_flags['extra_link_args'])
-ldf += ' %s' % os.environ.get('LDFLAGS', '')
-# filter rpath
-ldf = re.sub('-Wl,-rpath -Wl,[^ ]* ', '' , ldf, reflags)
-ldf = ldf.replace('  ', '').strip()
+ldf = ('%s %s' % (
+    ' '.join(compilation_flags['extra_link_args']),
+    os.environ.get('LDFLAGS', ''),
+)).replace('  ', '').strip()
+ldfp = ldf.split()
+ldfpp = []
+for idx, i in enumerate(ldfp[:]):
+    add = True
+    if i.startswith('-L/'):
+        if i.endswith('/bin'):
+            add = False
+    if i.startswith('-L/') or i.startswith('-l'):
+        if i in ldfpp:
+            add = False
+    if add:
+        ldfpp.append(i)
+ldf = ' '.join(ldfpp)
+## filter rpath
+# ldf = re.sub('-Wl,-rpath -Wl,[^ ]* ', '' , ldf, reflags)
+# ldf = ldf.replace('  ', '').strip()
 
 os.environ['LDFLAGS'] = os.environ['CFLAGS'] = ''
 
-# import pdb;pdb.set_trace()
-# pycairo does not play well with setuptools
-# the user may install it himself
-#for lib in compilation_flags['extra_link_args']:
-#    if 'cairo' in lib:
-#        dep = 'pycairo'
-#        if sys.version_info[0] < 3:
-#            dep = 'py2cairo'
-#        install_requires.append(dep)
-#        break
-
-# import pdb;pdb.set_trace()
 version = '2.1.0'
 if 'MAPNIK_DEBUG' in os.environ:
     summary(compilation_flags)
+    print "CFLAGS: %s\n" % cf
+    print "LDFLAGS: %s\n" % ldf
+
 install_requires=['setuptools',]
 test_requires = ['nose']
 setup(
